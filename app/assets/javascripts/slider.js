c7fd32b516835f01d22e1ba63af8f3d642ce030b
alexpro <tro_alex@mail.ru>
2015-09-14 10:48:44 +0400

    Rebuild carousel

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -4,51 +4,95 @@
 function Carousel(option) {
   var _this = $(this);
   var elem = option.elem,
-    newLeft = 0,
-    arrowButton = $(".arrow", elem),
+    leftPosition = 0,
+    innerBoxWidth = 0,
+    slicedPart = 0,
+    slicedPartRight = 0,
     arrowLeft = $(".prev", elem),
     arrowRight = $(".next", elem),
     carouselInnerBox = $(".carousel-inner-box", elem),
-    containerWidth = $(".container").width() + 8,
-    innerBoxWidth = 0;
+    containerWidth = 0,
+    leftPositionABS;
 
+  console.log("containerWidth = " + containerWidth);
 
+  //Init carousel
   setCarouselWidth();
+  setLeftPosition();
+  getCarouselWidth();
+  calcSlicedPart();
   toggleButton();
+
 
   // Set width for dynamic inner box
   function setCarouselWidth() {
-    var thumbsQuantity = $(".row .image-frame", elem).length;
-    innerBoxWidth = thumbsQuantity*240/2 + 496;
+    var thumbsQuantity = $(".image-frame", elem).length;
+    innerBoxWidth = thumbsQuantity*240/2 + 460;
     carouselInnerBox.width(innerBoxWidth + "px");
+    console.log("InnerBox = " + innerBoxWidth);
+  }
+
+  function getCarouselWidth() {
+    return containerWidth = parseInt($(".container").css("width"));
   }
 
   // Show arrow buttons when possible
   function toggleButton() {
-    newLeft == 0 ? arrowLeft.hide() : arrowLeft.show();
-    Math.abs(newLeft) >= innerBoxWidth - containerWidth ? arrowRight.hide() : arrowRight.show();
+    leftPosition == 0 ? arrowLeft.hide() : arrowLeft.show();
+    Math.abs(leftPosition) >= innerBoxWidth - containerWidth ? arrowRight.hide() : arrowRight.show();
   }
 
-  //Move slides left/right
-  _this.moveCarousel = function() {
+  // Calculate sliced part of carousel
+  function calcSlicedPart() {
+    return slicedPart = innerBoxWidth % containerWidth;
+  }
 
-    if($(this).hasClass("prev")) {
-      newLeft += containerWidth;
+  function setLeftPosition() {
+    carouselInnerBox.css("left", leftPosition + "px");
+  }
+
+
+  //Move slides left on prev button click
+  _this.moveLeft = function() {
+
+    if(slicedPart > 0 && Math.abs(leftPosition)+containerWidth == innerBoxWidth) {
+      leftPosition += slicedPart;
     } else {
-      newLeft -= containerWidth;
+      leftPosition += containerWidth;
     }
-    carouselInnerBox.css("left", newLeft + "px");
+
+    setLeftPosition();
     toggleButton();
   };
+  arrowLeft.on("click", _this.moveLeft);
 
-  arrowButton.on("click", _this.moveCarousel);
+
+
+  //Move slides right on next button click
+  _this.moveRight = function() {
+    leftPositionABS = Math.abs(leftPosition);
+
+    if (leftPositionABS == 0 && slicedPart > 0 && innerBoxWidth/containerWidth < 2) {
+      leftPosition -= slicedPart;
+    } else {
+      slicedPartRight = innerBoxWidth - 2*leftPositionABS;
+      leftPosition -= (slicedPartRight < containerWidth) ? slicedPart : containerWidth;
+    }
+
+    setLeftPosition();
+    toggleButton();
+  };
+  arrowRight.on("click", _this.moveRight);
 
 
   // Change Container(Carousel) width on window resize
   $(window).on("resize", function() {
-    containerWidth = $(".container").width() + 8;
-    newLeft = 0;
-    carouselInnerBox.css("left", newLeft + "px");
+    leftPosition = 0;
+
+    // Init carousel
+    getCarouselWidth();
+    calcSlicedPart();
+    setLeftPosition();
     toggleButton();
   });
 
